ee0c664d840fccdab095743b9de0b9b207838cbc
Nicholas Tillmann <nit36@pitt.edu>
2022-04-13 23:12:18 -0400

    having it so items added are now saved for state

--- src/components/Todo.js
+++ src/components/Todo.js
@@ -1,9 +1,9 @@
 import React from "react";
 
-const Todo = (setTodos, todos) => {
+const Todo = ({ text }) => {
     return (
         <div className="todo">
-            <ul className="todo-item"></ul>
+            <ul className="todo-item">{text}</ul>
             <button className="complete-btn"> <i className="fas fa-check"> </i></button>
             <button className="trash-btn"> <i className="fas fa-trash"> </i></button>
         </div >
--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -7,7 +7,7 @@ const TodoList = ({ todos }) => {
         <div className="todo-container">
             <ul className="todo-list">
                 {todos.map(todo => (
-                    <Todo todos={todos} />
+                    <Todo text={todo.text} id={todo.id} />
                 ))}
             </ul>
         </div>
