6bc76162c6bced65a9cf487cb97299fbb174b9c1
Nicholas Tillmann <ntillmann1439@gmail.com>
2022-04-15 13:22:01 -0400

    adding filterHandler for different task categories

--- src/App.js
+++ src/App.js
@@ -5,9 +5,26 @@ import { useState } from "react";
 
 
 function App() {
+  // state stuff
   const [inputText, setInputText] = useState("");
   const [todos, setTodos] = useState([]);
-  const [completed, setCompleted] = useState([]);
+  const [status, setStatus] = useState([]);
+  const [filteredTodos, setFilteredTodos] = useState([]);
+
+  // Functions
+  const filterHandler = () => {
+    switch (status) {
+      case 'completed':
+        setFilteredTodos(todos.filter(todo => todo.completed === true))
+        break
+      case 'uncompleted':
+        setFilteredTodos(todos.filter(todo => todo.completed === false))
+        break
+      case 'all':
+        setFilteredTodos(todos.filter(todos))
+        break
+    }
+  }
   return (
     <div className="App">
       <header>
